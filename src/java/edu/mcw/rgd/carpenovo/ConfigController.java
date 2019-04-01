0df55d53f4ceb0b98635389004cd5188bab6c57a
jt15 <jthota01@gmail.com>
2019-04-01 10:31:15 -0500

    added exception

--- src/java/edu/mcw/rgd/carpenovo/ConfigController.java
+++ src/java/edu/mcw/rgd/carpenovo/ConfigController.java
@@ -22,41 +22,23 @@ public class ConfigController extends HaplotyperController {
 
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         ArrayList errorList = new ArrayList();
-        try{
-        if(!checkRegionPositionBounds(request)) {
-            errorList.addAll(new ArrayList<>(Arrays.asList("Please reduce the region size to below 30MB")));
-            request.setAttribute("error", errorList);
-
-                Position p = getGeneSSLPRegion(request);
-                if (p != null) {
-                    request.setAttribute("error", errorList);
-                    request.setAttribute("start", p.getStart());
-                    request.setAttribute("stop", p.getStop());
-                    request.setAttribute("chr", p.getChromosome());
-                }
-
-
-                return new ModelAndView("/WEB-INF/jsp/haplotyper/region.jsp");
-
-        }
-        }catch (Exception e){
-            errorList.addAll(new ArrayList<>(Arrays.asList(e.getMessage())));
-            request.setAttribute("error", errorList);
-            return new ModelAndView("/WEB-INF/jsp/haplotyper/region.jsp");
-        }
 
         boolean positionSet=false;
         boolean strainsSet=false;
         boolean genesSet=false;
         boolean regionSet=false;
-
+        long region=0L;
 
         try {
             HttpRequestFacade req = new HttpRequestFacade(request);
 
             VariantSearchBean vsb = this.fillBean(req);
             request.setAttribute("vsb", vsb);
+            if ((vsb.getStopPosition() - vsb.getStartPosition()) > 30000000) {
+                region = (vsb.getStopPosition() - vsb.getStartPosition()) / 1000000;
+                throw new Exception("Maximum Region size is 30MB. Current region is " + region + "MB.");
 
+            }
 
             if (!req.getParameter("chr").equals("") && !req.getParameter("start").equals("") && !req.getParameter("stop").equals("")) {
                 positionSet = true;
@@ -99,6 +81,8 @@ public class ConfigController extends HaplotyperController {
 
         }catch (Exception e) {
             errorList.add(e.getMessage());
+            request.setAttribute("error", errorList);
+            return new ModelAndView("/WEB-INF/jsp/haplotyper/region.jsp");
         }
 
         request.setAttribute("error",errorList);
