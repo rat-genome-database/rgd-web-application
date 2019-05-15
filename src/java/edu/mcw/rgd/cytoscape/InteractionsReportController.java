4bf018a3801765a354dbf2cd619b999cc56c5701
Marek Tutaj <mtutaj@mcw.edu>
2019-05-15 08:08:35 -0500

    code cleanup for interactions report controller

--- src/java/edu/mcw/rgd/cytoscape/InteractionsReportController.java
+++ src/java/edu/mcw/rgd/cytoscape/InteractionsReportController.java
@@ -1,9 +1,6 @@
 package edu.mcw.rgd.cytoscape;
 
-import edu.mcw.rgd.datamodel.Interaction;
-import edu.mcw.rgd.datamodel.SpeciesType;
 import edu.mcw.rgd.reporting.Report;
-import edu.mcw.rgd.search.elasticsearch1.model.SearchBean;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
@@ -21,37 +18,20 @@ import java.util.Map;
  */
 public class InteractionsReportController implements Controller {
     Map<String, List<InteractionReportRecord>> map= new HashMap<>();
-    Map<String, Report> reportMap= new HashMap<>();
- /* @Override
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-      String species= request.getParameter("species");
-      InteractionsService service= new InteractionsService();
-        Report report= service.getInteractionsBySpecies(species);
-        if(reportMap.get(species.toLowerCase())!=null){
-            report=reportMap.get(species);
-        }else{
-            report= service.getInteractionsBySpecies(species);
-            reportMap.put(species.toLowerCase(), report);
-        }
+    //Map<String, Report> reportMap= new HashMap<>();
 
-
-      request.setAttribute("report", report);
-      request.setAttribute("species", species);
-      return new ModelAndView("/WEB-INF/jsp/cytoscape/report.jsp");
-    }
-*/
- @Override
+    @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String species = request.getParameter("species");
-         HttpSession session = request.getSession();
+        HttpSession session = request.getSession();
         InteractionsService service = new InteractionsService();
         ModelMap model = new ModelMap();
-        List<InteractionReportRecord> records= new ArrayList<>();
-        if(map.get(species.toLowerCase())!=null){
-            records=map.get(species.toLowerCase());
+        List<InteractionReportRecord> records;
+        if( map.get(species.toLowerCase())!=null ){
+            records = map.get(species.toLowerCase());
 
-        }else{
-            records= service.getInteractionsBySpecies(species);
+        } else {
+            records = service.getInteractionsBySpecies(species);
             map.put(species.toLowerCase(), records);
         }
         session.setAttribute("records", records);
