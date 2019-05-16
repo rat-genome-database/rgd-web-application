99097120cbe367af5f76cd3dcf31affdb2100afb
Marek Tutaj <mtutaj@mcw.edu>
2019-05-16 13:00:32 -0500

    gene/qtl/strain object edit tool should always generate appropriate nomenclature events and aliases when symbol/name changes -- per RGDD-313

--- src/java/edu/mcw/rgd/edit/QTLEditObjectController.java
+++ src/java/edu/mcw/rgd/edit/QTLEditObjectController.java
@@ -11,16 +11,15 @@ import java.util.List;
 import java.util.Date;
 
 /**
- * Created by IntelliJ IDEA.
- * User: jdepons
- * Date: Jun 2, 2008
- * Time: 8:59:47 AM
+ * @author jdepons
+ * @since Jun 2, 2008
  */
 public class QTLEditObjectController extends EditObjectController {
 
+    QTLDAO dao = new QTLDAO();
+
     public String getViewUrl() throws Exception {
        return "editQTL.jsp";
-
     }
 
     public int getObjectTypeKey() {
@@ -28,11 +27,13 @@ public class QTLEditObjectController extends EditObjectController {
     }
     
     public Object getObject(int rgdId) throws Exception{        
-        return new QTLDAO().getQTL(rgdId);                           
+        return dao.getQTL(rgdId);
     }
+
     public Object getSubmittedObject(int submissionKey) throws Exception {
         return null;
     }
+
     public Object newObject() throws Exception{
         QTL qtl = new QTL();
         qtl.setRgdId(-1);        
@@ -41,122 +42,120 @@ public class QTLEditObjectController extends EditObjectController {
     }
 
     public Object update(HttpServletRequest request, boolean persist) throws Exception {
-         HttpRequestFacade req = new HttpRequestFacade(request);
-         QTLDAO dao = new QTLDAO();
-         List<NomenclatureEvent> nomenEvents = new ArrayList<>();
-         QTL qtl = null;
+        HttpRequestFacade req = new HttpRequestFacade(request);
+        if (req.getParameter("key").equals("")) {
+            return null;
+        }
 
-         boolean isNew = false;
+        List<NomenclatureEvent> nomenEvents = new ArrayList<>();
+        List<Alias> aliases = new ArrayList<>();
 
-         if (!req.getParameter("key").equals("")) {
+        boolean isNew = false;
 
-             String symbol = req.getParameter("symbol");
-             this.checkSet("Symbol", symbol);
+        String symbol = req.getParameter("symbol");
+        this.checkSet("Symbol", symbol);
 
-             String name = req.getParameter("name");
-             this.checkSet("Name", name);
+        String name = req.getParameter("name");
+        this.checkSet("Name", name);
 
-             int rgdId = Integer.parseInt(req.getParameter("rgdId"));
+        int rgdId = Integer.parseInt(req.getParameter("rgdId"));
 
-             if (rgdId == -1 ) {
-                 isNew = true;
-                 qtl = new QTL();
-             }else {
-                 qtl = dao.getQTL(rgdId);
+        QTL qtl;
+        if (rgdId == -1) {
+            isNew = true;
+            qtl = new QTL();
+        } else {
+            qtl = dao.getQTL(rgdId);
 
-                 if( !Utils.stringsAreEqual(qtl.getSymbol(), symbol) ) {
-                     NomenclatureEvent ne = new NomenclatureEvent();
-                     ne.setDesc("Symbol updated");
-                     ne.setEventDate(new Date());
-                     ne.setName(name);
-                     ne.setSymbol(symbol);
-                     ne.setNomenStatusType("APPROVED");
-                     ne.setOriginalRGDId(qtl.getRgdId());
-                     ne.setPreviousName(qtl.getName());
-                     ne.setPreviousSymbol(qtl.getSymbol());
-                     ne.setRefKey("627");
-                     ne.setRgdId(rgdId);
-                     nomenEvents.add(ne);
-                 }
+            if (!Utils.stringsAreEqual(qtl.getSymbol(), symbol) || !Utils.stringsAreEqual(qtl.getName(), name)) {
+                NomenclatureEvent ne = new NomenclatureEvent();
+                ne.setDesc("Symbol and/or name change");
+                ne.setEventDate(new Date());
+                ne.setName(name);
+                ne.setSymbol(symbol);
+                ne.setNomenStatusType("APPROVED");
+                ne.setOriginalRGDId(qtl.getRgdId());
+                ne.setPreviousName(qtl.getName());
+                ne.setPreviousSymbol(qtl.getSymbol());
+                ne.setRefKey("853");
+                ne.setRgdId(rgdId);
+                nomenEvents.add(ne);
 
-                 if( !Utils.stringsAreEqual(qtl.getName(), name) ) {
-                     NomenclatureEvent ne = new NomenclatureEvent();
-                     ne.setDesc("Name updated");
-                     ne.setEventDate(new Date());
-                     ne.setName(name);
-                     ne.setSymbol(symbol);
-                     ne.setNomenStatusType("APPROVED");
-                     ne.setOriginalRGDId(qtl.getRgdId());
-                     ne.setPreviousName(qtl.getName());
-                     ne.setPreviousSymbol(qtl.getSymbol());
-                     ne.setRefKey("853");
-                     ne.setRgdId(rgdId);
-                     nomenEvents.add(ne);
-                 }
-             }
+                if( !Utils.stringsAreEqual(qtl.getSymbol(), symbol) ) {
+                    Alias alias = new Alias();
+                    alias.setRgdId(rgdId);
+                    alias.setTypeName("old_qtl_symbol");
+                    alias.setValue(qtl.getSymbol());
+                    alias.setNotes("created by QTL Edit on "+new Date());
+                    aliases.add(alias);
+                }
 
-             qtl.setSymbol(req.getParameter("symbol"));
-             qtl.setName(req.getParameter("name"));
+                if( !Utils.stringsAreEqual(qtl.getName(), name) ) {
+                    Alias alias = new Alias();
+                    alias.setRgdId(rgdId);
+                    alias.setTypeName("old_qtl_name");
+                    alias.setValue(qtl.getName());
+                    alias.setNotes("created by QTL Edit on "+new Date());
+                    aliases.add(alias);
+                }
+            }
+        }
 
-             if (this.checkInteger("Peak Offset", req.getParameter("peakOffset"), false)) {
-                 qtl.setPeakOffset(Integer.parseInt(req.getParameter("peakOffset")));
-             }
+        qtl.setSymbol(symbol);
+        qtl.setName(name);
 
-             if (this.checkChromosome(req.getParameter("chromosome"), false)) {
-                qtl.setChromosome(req.getParameter("chromosome"));
-             }
+        if (this.checkInteger("Peak Offset", req.getParameter("peakOffset"), false)) {
+            qtl.setPeakOffset(Integer.parseInt(req.getParameter("peakOffset")));
+        }
 
-             if (this.checkNumeric("LOD", req.getParameter("lod"), false)) {
-                qtl.setLod(Double.parseDouble(req.getParameter("lod")));
-             } else {
-                 qtl.setLod(null);
-             }
+        if (this.checkChromosome(req.getParameter("chromosome"), false)) {
+            qtl.setChromosome(req.getParameter("chromosome"));
+        }
 
-             if (this.checkNumeric("P Value", req.getParameter("pValue"), false)) {
-                 qtl.setPValue(Double.parseDouble(req.getParameter("pValue")));
-             } else {
-                 qtl.setPValue(null);
-             }
+        if (this.checkNumeric("LOD", req.getParameter("lod"), false)) {
+            qtl.setLod(Double.parseDouble(req.getParameter("lod")));
+        } else {
+            qtl.setLod(null);
+        }
 
-             if (this.checkNumeric("Variance", req.getParameter("variance"), false)) {
-                 qtl.setVariance(Double.parseDouble(req.getParameter("variance")));
-             } else {
-                 qtl.setVariance(null);
-             }
+        if (this.checkNumeric("P Value", req.getParameter("pValue"), false)) {
+            qtl.setPValue(Double.parseDouble(req.getParameter("pValue")));
+        } else {
+            qtl.setPValue(null);
+        }
 
-             if (this.checkInteger("Flank 1 RGD ID",req.getParameter("flank1RgdId"), false)) {
-                 RGDManagementDAO rmdao = new RGDManagementDAO();
-                 rmdao.getRgdId(Integer.parseInt(req.getParameter("flank1RgdId")));
-                 qtl.setFlank1RgdId(Integer.parseInt(req.getParameter("flank1RgdId")));
-             }
-             if (this.checkInteger("Flank 2 RGD ID",req.getParameter("flank2RgdId"), false)) {
-                 RGDManagementDAO rmdao = new RGDManagementDAO();
-                 rmdao.getRgdId(Integer.parseInt(req.getParameter("flank2RgdId")));
+        if (this.checkNumeric("Variance", req.getParameter("variance"), false)) {
+            qtl.setVariance(Double.parseDouble(req.getParameter("variance")));
+        } else {
+            qtl.setVariance(null);
+        }
 
-                 qtl.setFlank2RgdId(Integer.parseInt(req.getParameter("flank2RgdId")));
-             }
-             if (this.checkInteger("Peak RGD ID",req.getParameter("peakRgdId"), false)) {
-                 RGDManagementDAO rmdao = new RGDManagementDAO();
-                 rmdao.getRgdId(Integer.parseInt(req.getParameter("peakRgdId")));
+        if (this.checkInteger("Flank 1 RGD ID", req.getParameter("flank1RgdId"), false)) {
+            qtl.setFlank1RgdId(Integer.parseInt(req.getParameter("flank1RgdId")));
+        }
+        if (this.checkInteger("Flank 2 RGD ID", req.getParameter("flank2RgdId"), false)) {
+            qtl.setFlank2RgdId(Integer.parseInt(req.getParameter("flank2RgdId")));
+        }
 
-                 qtl.setPeakRgdId(Integer.parseInt(req.getParameter("peakRgdId")));
-             }
+        if (this.checkInteger("Peak RGD ID", req.getParameter("peakRgdId"), false)) {
+            qtl.setPeakRgdId(Integer.parseInt(req.getParameter("peakRgdId")));
+        }
 
-             qtl.setInheritanceType(req.getParameter("inheritanceType"));
-             qtl.setLodImage(req.getParameter("lodImage"));
-             qtl.setLinkageImage(req.getParameter("linkageImage"));
-             qtl.setSourceUrl(req.getParameter("sourceUrl"));
-             qtl.setMostSignificantCmoTerm(req.getParameter("mostSignificantCmoTerm"));
+        qtl.setInheritanceType(req.getParameter("inheritanceType"));
+        qtl.setLodImage(req.getParameter("lodImage"));
+        qtl.setLinkageImage(req.getParameter("linkageImage"));
+        qtl.setSourceUrl(req.getParameter("sourceUrl"));
+        qtl.setMostSignificantCmoTerm(req.getParameter("mostSignificantCmoTerm"));
 
-             if (persist) {
-                 if (isNew) {
-                    dao.insertQTL(qtl, req.getParameter("objectStatus"),SpeciesType.parse(req.getParameter("speciesType")) );
-                 } else {
-                    dao.updateQTL(qtl);
-                    this.addNomenEvents(nomenEvents);
-                 }
-             }
-         }
+        if (persist) {
+            if (isNew) {
+                dao.insertQTL(qtl, req.getParameter("objectStatus"), SpeciesType.parse(req.getParameter("speciesType")));
+            } else {
+                dao.updateQTL(qtl);
+                addNomenEvents(nomenEvents);
+                insertAliases(aliases);
+            }
+        }
         return qtl;
     }
 }
