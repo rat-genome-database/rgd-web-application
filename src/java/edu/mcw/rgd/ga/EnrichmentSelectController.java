82000a323a7479c9c82856fd9507ed187d55f9b7
harika91 <harika91@users.noreply.github.com>
2019-06-06 08:06:02 -0500

    Changes to add species in request

--- src/java/edu/mcw/rgd/ga/EnrichmentSelectController.java
+++ src/java/edu/mcw/rgd/ga/EnrichmentSelectController.java
@@ -23,6 +23,8 @@ public class EnrichmentSelectController extends GAController {
         request.setAttribute("status", status);
         request.setAttribute("warn", warning);
 
-        return new ModelAndView("/WEB-INF/jsp/enrichment/start.jsp", "hello", null);
+        if(request.getParameter("species") != null)
+        return new ModelAndView("/WEB-INF/jsp/enrichment/start.jsp?species="+request.getParameter("species"), "hello", null);
+        else return new ModelAndView("/WEB-INF/jsp/enrichment/start.jsp?species=3", "hello", null);
     }
 }
