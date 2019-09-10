714f6c6f9f974e5a2e05810aa3ae8084a0240d99
harika91 <harika91@users.noreply.github.com>
2019-09-10 10:26:30 -0500

    Add logout controller

---
+++ src/java/edu/mcw/rgd/edit/CurationLogoutController.java
@@ -0,0 +1,29 @@
+package edu.mcw.rgd.edit;
+
+
+import edu.mcw.rgd.process.FileDownloader;
+import org.json.JSONObject;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class CurationLogoutController implements Controller {
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+            Cookie cookie = new Cookie("accessToken", "");
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+            response.addHeader("Cache-Control","max-age=5, must-revalidate");
+            return new ModelAndView("/WEB-INF/jsp/curation/logout.jsp", "hello", null);
+        }
+
+}
