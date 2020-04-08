9ec3cc2c92f44506401c3f2db416c061af3cc86a
harika91 <harika91@users.noreply.github.com>
2020-04-07 19:43:35 -0500

    Update StrainFileUploadController.java

--- src/java/edu/mcw/rgd/edit/StrainFileUploadController.java
+++ src/java/edu/mcw/rgd/edit/StrainFileUploadController.java
@@ -4,7 +4,6 @@ import edu.mcw.rgd.dao.impl.StrainDAO;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
@@ -18,14 +17,12 @@ public class StrainFileUploadController implements Controller {
         if(request.getParameter("fileType") != null){
             int strainId = Integer.parseInt(request.getParameter("strainId"));
             String type = request.getParameter("fileType");
-            String desc = request.getParameter("description");
             Part file = request.getPart("file");
             if (file != null) {
                 InputStream data = file.getInputStream();
                 StrainDAO dao =new StrainDAO();
-                dao.insertStrainAttachment(strainId,type,data,desc);
+                dao.insertStrainAttachment(strainId,type,data,file.getContentType());
             }
-            System.out.println(type);
         }
         return new ModelAndView("/WEB-INF/jsp/strainFileUpload.jsp");
     }
