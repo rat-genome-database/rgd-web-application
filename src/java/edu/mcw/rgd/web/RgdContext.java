a3cc90b7fce0a6ca1160f01fd12c10e8c87fe30a
jt15 <jthota01@gmail.com>
2020-04-20 12:05:26 -0500

    updated

--- src/java/edu/mcw/rgd/web/RgdContext.java
+++ src/java/edu/mcw/rgd/web/RgdContext.java
@@ -104,7 +104,7 @@ public class RgdContext {
                 return index+"_index_prod";
 			}
 			if( isPipelines() ) {
-				return "rgd_index_cur";
+				return index+"_index_cur";
              //   return index+"_index_prod";
 			}
 			if( isTest() ) {
