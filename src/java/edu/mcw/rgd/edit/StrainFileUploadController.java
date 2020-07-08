7d2bcdf15ed1a19f79f9409a0b46446b557de542
harika91 <harika91@users.noreply.github.com>
2020-07-08 08:16:41 -0500

    Changes to upload strain files

--- src/java/edu/mcw/rgd/edit/StrainFileUploadController.java
+++ src/java/edu/mcw/rgd/edit/StrainFileUploadController.java
@@ -1,18 +1,23 @@
 package edu.mcw.rgd.edit;
 
 import edu.mcw.rgd.dao.impl.StrainDAO;
+import org.json.JSONObject;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
 public class StrainFileUploadController implements Controller {
-
+    String login = "";
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         ArrayList error = new ArrayList();
@@ -20,7 +25,14 @@ public class StrainFileUploadController implements Controller {
         ArrayList status = new ArrayList();
         StrainDAO dao =new StrainDAO();
         int strainId = 0;
-        boolean report = false;
+        if(request.getCookies() != null && request.getCookies().length != 0)
+            if(request.getCookies()[0].getName().equalsIgnoreCase("accessToken")) {
+                String accessToken = request.getCookies()[0].getValue();
+                if(!checkToken(accessToken)) {
+                    response.sendRedirect("https://github.com/login/oauth/authorize?client_id=7de10c5ae2c3e3825007&scope=user&redirect_uri=https://dev.rgd.mcw.edu/rgdweb/curation/login.html");
+                    return null;
+                }
+            }
         try{
         if(request.getParameter("strainId") != null){
             strainId = Integer.parseInt(request.getParameter("strainId"));
@@ -40,14 +52,14 @@ public class StrainFileUploadController implements Controller {
                                     InputStream data = file.getInputStream();
                                     String contentType = dao.getContentType(strainId, type);
                                     if (contentType == null) {
-                                        dao.insertStrainAttachment(strainId, type, data, file.getContentType(), fileName);
+                                        dao.insertStrainAttachment(strainId, type, data, file.getContentType(), fileName,login);
                                         status.add("File Uploaded Successfully for strain " + strainId + " and " + type);
-                                        report = true;
+
                                     } else {
-                                        dao.updateStrainAttachment(strainId, type, data, file.getContentType(), fileName);
+                                        dao.updateStrainAttachment(strainId, type, data, file.getContentType(), fileName,login);
                                         warning.add("File already Exists for strain " + strainId + " and " + type);
                                         status.add("File Replaced Successfully for strain " + strainId + " and "+ type);
-                                        report = true;
+
                                     }
                                 }
                              }
@@ -83,9 +95,34 @@ public class StrainFileUploadController implements Controller {
         request.setAttribute("warn", warning);
 
 
-        if(report)
-            return new ModelAndView("/WEB-INF/jsp/report/strain/main.html?id="+strainId);
+
         return new ModelAndView("/WEB-INF/jsp/curation/strainFileUpload.jsp");
     }
+    protected boolean checkToken(String token) throws Exception{
+        if(token == null || token.isEmpty()){
+            return false;
+        }else {
+            URL url = new URL("https://api.github.com/user");
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+            conn.setRequestProperty("Authorization", "Token "+token);
 
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream())) ) {
+                String line = in.readLine();
+                JSONObject json = new JSONObject(line);
+                login = (String)json.get("login");
+                if(!login.equals("")){
+                    URL checkUrl = new URL("https://api.github.com/orgs/rat-genome-database/members/"+login);
+                    HttpURLConnection connection = (HttpURLConnection)checkUrl.openConnection();
+                    connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+                    connection.setRequestProperty("Authorization", "Token "+token);
+                    if(connection.getResponseCode()== 204)
+                        return true;
+                }
+            }
+
+
+            return false;
+        }
+    }
 }
