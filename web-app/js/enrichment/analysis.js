2f402e4ab371b888952ecb3b236078c9fefd5baf
De Pons <jdepons@rgd-dep-mac.local>
2021-06-23 12:25:02 -0500

    updated

--- web-app/js/enrichment/analysis.js
+++ web-app/js/enrichment/analysis.js
@@ -30,6 +30,7 @@ function EnrichmentVue(divId, hostname) {
             info: [],
             hostName: host,
             species: [],
+            originalSpecies:0,
             ontology: [],
             allSpecies: ["Rat", "Human", "Mouse", "Dog", "Squirrel", "Bonobo", "Chinchilla","Pig","Naked Mole-rat","Green Monkey"],
             allOntologies: ["RDO", "PW", "BP", "CC", "MF", "MP", "CHEBI"],
@@ -41,6 +42,7 @@ function EnrichmentVue(divId, hostname) {
             pvalueLimit: 0.05,
             geneData: {},
             genes: [],
+            originalGenes: [],
             currentSort: 'pvalue',
             currentSortDir: 'asc',
             selectedAll: false,
@@ -118,6 +120,8 @@ function EnrichmentVue(divId, hostname) {
                 document.getElementById(v.ontology[0]).innerHTML = "";
               v.ontology = [ont];
               v.species = [species];
+              v.originalSpecies = species;
+              v.originalGenes = genes;
               v.genes = genes;
               v.graph = graph;
               v.table = table;
