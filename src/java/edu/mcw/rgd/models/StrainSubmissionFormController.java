a4d638c14dbd390c729109c435e5e18a2151b6f3
Logan Lamers <llamers@mcw.edu>
2021-11-15 09:21:59 -0600

    strain submission email fix

--- src/java/edu/mcw/rgd/models/StrainSubmissionFormController.java
+++ src/java/edu/mcw/rgd/models/StrainSubmissionFormController.java
@@ -167,8 +167,8 @@ public class StrainSubmissionFormController implements Controller {
                    String curatorMsg="Dear RGD Curators, \n\nA new strain registration information has been generated.\n\n" +
                            "Submitter Name:  "+ firstName + " "+ lastName+"\nSubmitter Email Address:  " + submitterEmail + "\nOrganization:  " + org+"\nPI Name:  "+piName+"\nSource:  "+source+"\nStatus:  "+status+"\nSubmission Key:  "+insertedCount +"\nLink to Submitted Strains Interface: " +link;
                     MyRGDLookupController.send(submitterEmail, "Strain Submission", userMsg   );
-                   // MyRGDLookupController.send("rgd.data2@mcw.edu", "New Strain Submission", curatorMsg);
-                    MyRGDLookupController.send("jthota@mcw.edu", "New Strain Submission", curatorMsg);
+                    MyRGDLookupController.send("rgd.data2@mcw.edu", "New Strain Submission", curatorMsg);
+//                    MyRGDLookupController.send("jthota@mcw.edu", "New Strain Submission", curatorMsg);
                     response.getWriter().write(msg);
                     return null;
                 }
