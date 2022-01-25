d52a240ded86a6a2e3a757922b4b11ead50a3915
tutajm <mtutaj@mcw.edu>
2022-01-24 19:58:05 -0600

    gene merge: now map positions are transferred to target genes

--- src/java/edu/mcw/rgd/edit/GeneMergeController.java
+++ src/java/edu/mcw/rgd/edit/GeneMergeController.java
@@ -421,6 +421,17 @@ public class GeneMergeController implements Controller {
 
         if( !bean.getMapDataNew().isEmpty() ) {
             MapDAO mapDAO = new MapDAO();
+
+            for( MapData md: bean.getMapDataNew() ) {
+                String notes = "created by GeneMerge from RGD ID " + bean.getRgdIdFrom().getRgdId();
+                if (md.getNotes() == null)
+                    md.setNotes(notes);
+                else
+                    md.setNotes(md.getNotes() + "; " + notes);
+
+                md.setRgdId(bean.getRgdIdTo().getRgdId());
+            }
+
             mapDAO.insertMapData(bean.getMapDataNew());
         }
     }
