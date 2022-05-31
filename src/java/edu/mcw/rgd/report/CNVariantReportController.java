3b98ebcaaf7026ce9972bd3e89a16508e7e1cea9
llamersmcw <llamers@mcw.edu>
2022-05-31 11:58:12 -0500

    added rsId page that list all variants with given id

--- src/java/edu/mcw/rgd/report/CNVariantReportController.java
+++ src/java/edu/mcw/rgd/report/CNVariantReportController.java
@@ -1,15 +1,8 @@
 package edu.mcw.rgd.report;
 
 import edu.mcw.rgd.dao.impl.variants.VariantDAO;
-import edu.mcw.rgd.web.HttpRequestFacade;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-
-public class CNVariantReportController implements Controller {
+public class CNVariantReportController extends ReportController {
 
     public String getViewUrl() throws Exception {
         return "cnVariants/main.jsp";
@@ -17,72 +10,5 @@ public class CNVariantReportController implements Controller {
 
     public Object getObject(int rgdId) throws Exception{
         return new VariantDAO().getVariant(rgdId);
-    }
-
-    public Object getObject(String rsId) throws Exception{
-        return new VariantDAO().getVariantByRsId(rsId);
-    }
-
-    protected HttpServletRequest request = null;
-    protected HttpServletResponse response = null;
-
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        this.request = request;
-        this.response = response;
-
-        ArrayList error = new ArrayList();
-        ArrayList warning = new ArrayList();
-        ArrayList status = new ArrayList();
-        String path = "/WEB-INF/jsp/report/";
-
-        HttpRequestFacade req = new HttpRequestFacade(request);
-
-        String strRgdId = req.getParameter("id");
-        strRgdId = strRgdId.replaceAll("RGD:", "");
-        strRgdId = strRgdId.replaceAll("\\)","");
-
-        int rgdId=0;
-        Object object = null;
-
-        if (strRgdId.startsWith("rs")){
-            try{
-                object = getObject(strRgdId);
-                if( object==null ) {
-                    error.add("Invalid rs ID for this type of object!");
-                }
-            }
-            catch (Exception e){
-                error.add("Invalid rs IS for this object!");
-            }
-        }
-        else {
-            try {
-                rgdId = Integer.parseInt(strRgdId);
-                try {
-                    object = getObject(rgdId);
-                    if (object == null) {
-                        error.add("Invalid RGD ID for this type of object!");
-                    }
-                } catch (Exception e) {
-                    error.add(e.getMessage());
-                }
-            } catch (Exception e) {
-                error.add("RGD ID must be Numeric. Please Search Again.");
-            }
-        }
-
-        request.setAttribute("reportObject", object);
-        request.setAttribute("requestFacade", req);
-
-        request.setAttribute("error", error);
-        request.setAttribute("status", status);
-        request.setAttribute("warn", warning);
-
-
-        if (error.size() > 0) {
-            return new ModelAndView("/WEB-INF/jsp/search/searchByPosition.jsp");
-        }else {
-            return new ModelAndView(path + getViewUrl());
-        }
     }
 }
---
+++ src/java/edu/mcw/rgd/report/CNVariantsRsIdController.java
@@ -0,0 +1,58 @@
+package edu.mcw.rgd.report;
+
+import edu.mcw.rgd.dao.impl.variants.VariantDAO;
+import edu.mcw.rgd.datamodel.variants.VariantMapData;
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import edu.mcw.rgd.web.HttpRequestFacade;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class CNVariantsRsIdController implements Controller {
+    protected VariantDAO vdao = new VariantDAO();
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ArrayList error = new ArrayList();
+        ArrayList warning = new ArrayList();
+        ArrayList status = new ArrayList();
+        String path = "/WEB-INF/jsp/report/";
+
+        HttpRequestFacade req = new HttpRequestFacade(request);
+
+        String rsId = req.getParameter("id");
+        List<VariantMapData> objects = null;
+
+        try {
+            objects = vdao.getAllVariantByRsId(rsId);
+            if( objects==null ) {
+                error.add("Invalid rs ID for this type of object!");
+            }else if(objects.isEmpty()){
+                error.add("No variants with given rs ID!");
+            }
+        }
+        catch( Exception e ) {
+            error.add(e.getMessage());
+        }
+
+
+        request.setAttribute("reportObjects", objects);
+        request.setAttribute("requestFacade", req);
+
+        request.setAttribute("error", error);
+        request.setAttribute("status", status);
+        request.setAttribute("warn", warning);
+
+
+        if (error.size() > 0) {
+            return new ModelAndView("/WEB-INF/jsp/search/searchByPosition.jsp");
+        } else if (objects.size()==1){
+            request.setAttribute("reportObject", objects.get(0));
+            return new ModelAndView("/WEB-INF/jsp/report/cnVariants/main.jsp");
+        } else{
+            return new ModelAndView("/WEB-INF/jsp/report/rsIds/main.jsp");
+        }
+    }
+}
