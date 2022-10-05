b6ca1c050d439fa51e6d84fc22ece12f5ce5f72e
llamersmcw <llamers@mcw.edu>
2022-10-05 13:34:54 -0500

    Revert "Revert "releasing Variant report pages""
    This reverts commit e45a9ec023da77ab57aca1e0b367362b4b213b20.

---
+++ src/java/edu/mcw/rgd/report/VariantDownloadController.java
@@ -0,0 +1,40 @@
+package edu.mcw.rgd.report;
+
+import org.json.JSONObject;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+
+public class VariantDownloadController implements Controller {
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        try{
+            StringBuilder buffer = new StringBuilder();
+            BufferedReader reader = request.getReader();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+                buffer.append(System.lineSeparator());
+            }
+            JSONObject obj = new JSONObject(buffer.toString());
+            reader.close();
+            int start = Integer.parseInt(obj.get("start").toString());
+            int stopPos = Integer.parseInt(obj.get("stopPos").toString());
+            String chr = obj.get("chr").toString();
+            int mapKey = Integer.parseInt(obj.get("mapKey").toString());
+            String symbol = obj.get("symbol").toString();
+            request.setAttribute("start",start);
+            request.setAttribute("stopPos",stopPos);
+            request.setAttribute("chr",chr);
+            request.setAttribute("symbol",symbol);
+            request.setAttribute("mapKey",mapKey);
+        }
+        catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+        return new ModelAndView("/WEB-INF/jsp/report/rsIds/downloadVariants.jsp");
+    }
+}
