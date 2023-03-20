216002d59df9ba54ddaca2edddb17418d8028e2c
Thota <jthota@mcw.edu>
2023-03-20 16:35:15 -0500

    code clean up

--- src/java/edu/mcw/rgd/report/ClinVarReportController.java
+++ src/java/edu/mcw/rgd/report/ClinVarReportController.java
@@ -1,22 +1,6 @@
 package edu.mcw.rgd.report;
 
-import edu.mcw.rgd.dao.DataSourceFactory;
-import edu.mcw.rgd.dao.impl.VariantInfoDAO;
-import edu.mcw.rgd.dao.impl.variants.CarpenovoVariantInfoDAO;
 import edu.mcw.rgd.dao.impl.variants.VariantDAO;
-import edu.mcw.rgd.datamodel.SearchResult;
-import edu.mcw.rgd.datamodel.SpeciesType;
-import edu.mcw.rgd.datamodel.VariantResult;
-import edu.mcw.rgd.datamodel.VariantSearchBean;
-import edu.mcw.rgd.datamodel.variants.VariantIndex;
-import edu.mcw.rgd.vv.DetailController;
-import edu.mcw.rgd.vv.VariantController;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Created by IntelliJ IDEA.
  * User: mtutaj
