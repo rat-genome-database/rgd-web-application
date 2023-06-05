71a1d2578d5f58d5937751606923f3ec8336609e
Thota <jthota@mcw.edu>
2023-06-05 11:44:28 -0500

    reverted phenominer github stuff

--- src/java/edu/mcw/rgd/phenominer/PhenominerExperimentController.java
+++ src/java/edu/mcw/rgd/phenominer/PhenominerExperimentController.java
@@ -43,8 +43,8 @@ public class PhenominerExperimentController extends PhenominerController {
             if(request.getCookies()[0].getName().equalsIgnoreCase("accessToken")) {
                 String accessToken = request.getCookies()[0].getValue();
                 if(!checkToken(accessToken)) {
-                  //  response.sendRedirect("https://github.com/login/oauth/authorize?client_id=dc5513384190f8a788e5&scope=user&redirect_uri=https://pipelines.rgd.mcw.edu/rgdweb/curation/login.html");
-                 response.sendRedirect(RgdContext.getGithubOauthRedirectUrl());
+                   response.sendRedirect("https://github.com/login/oauth/authorize?client_id=dc5513384190f8a788e5&scope=user&redirect_uri=https://pipelines.rgd.mcw.edu/rgdweb/curation/login.html");
+                // response.sendRedirect(RgdContext.getGithubOauthRedirectUrl());
                     return null;
                 }
             }
