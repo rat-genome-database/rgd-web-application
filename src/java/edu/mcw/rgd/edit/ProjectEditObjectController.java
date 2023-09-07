ff4c36b8c0638cf9e906632563677cb7dfbe1379
Akhilanand K <akundurthi@mcw.edu>
2023-09-07 12:38:09 -0500

    Modifed Project edit object

--- src/java/edu/mcw/rgd/edit/ProjectEditObjectController.java
+++ src/java/edu/mcw/rgd/edit/ProjectEditObjectController.java
@@ -2,13 +2,15 @@ package edu.mcw.rgd.edit;
 
 import edu.mcw.rgd.dao.impl.RGDManagementDAO;
 import edu.mcw.rgd.dao.impl.XdbIdDAO;
+import edu.mcw.rgd.datamodel.Project;
 import edu.mcw.rgd.datamodel.Reference;
 import edu.mcw.rgd.dao.impl.ReferenceDAO;
+import edu.mcw.rgd.dao.impl.ProjectDAO;
 import edu.mcw.rgd.datamodel.RgdId;
 import edu.mcw.rgd.datamodel.XdbId;
 import edu.mcw.rgd.process.Utils;
 import edu.mcw.rgd.web.HttpRequestFacade;
-
+import edu.mcw.rgd.dao.impl.ProjectDAO;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
@@ -18,17 +20,18 @@ import java.util.List;
 public class ProjectEditObjectController extends EditObjectController {
 
     ReferenceDAO dao = new ReferenceDAO();
-
+    ProjectDAO prodao = new ProjectDAO();
     public String getViewUrl() throws Exception {
         return "editProject.jsp";
     }
 
     public int getObjectTypeKey() {
-        return 12;
+        return 14;
     }
 
     public Object getObject(int rgdId) throws Exception{
-        return dao.getReferenceByRgdId(rgdId);
+        return prodao.getProjectByRgdId1(rgdId);
+//        return dao.getReferenceByRgdId(rgdId);
     }
 
     public Object getSubmittedObject(int submissionKey) throws Exception {
@@ -36,88 +39,100 @@ public class ProjectEditObjectController extends EditObjectController {
     }
 
     public Object newObject() throws Exception{
-        Reference ref = new Reference();
-        ref.setReferenceType("DIRECT DATA TRANSFER");
-        return ref;
+//        Reference ref = new Reference();
+        Project pro = new Project();
+//        ref.setReferenceType("DIRECT DATA TRANSFER");
+        return pro;
     }
 
     public Object update(HttpServletRequest request, boolean persist) throws Exception {
         HttpRequestFacade req = new HttpRequestFacade(request);
         String rgdId = req.getParameter("rgdId");
-        Reference ref = null;
-        if( !rgdId.isEmpty() ) {
+//        int rgdId=Integer.parseInt(req.getParameter("rgdId"));
+//        Reference ref = null;
+        Project pro = null;
+        if( !rgdId.isEmpty()) {
             try {
-                ref = (Reference) getObject(Integer.parseInt(rgdId));
+//                ref = (Reference) getObject(Integer.parseInt(rgdId));
+                pro=(Project) getObject(Integer.parseInt(rgdId));
             } catch (Exception e) {
 
             }
         }
-        boolean isNew = ref==null;
+//        boolean isNew = ref==null;
+        boolean isNew = pro==null;
 
         if( isNew ) {
-            ref = (Reference) newObject();
+//            ref=(Reference) newObject();
+            pro = (Project) newObject();
         }
 
-        ref.setTitle(req.getParameter("title"));
-        ref.setEditors(req.getParameter("editors"));
-        ref.setPublication(req.getParameter("publication"));
-        ref.setVolume(req.getParameter("volume"));
-        ref.setIssue(req.getParameter("issue"));
-        ref.setPages(req.getParameter("pages"));
-        ref.setNotes(req.getParameter("notes"));
-        ref.setReferenceType(req.getParameter("referenceType"));
-        ref.setCitation(req.getParameter("citation"));
-        ref.setRefAbstract(req.getParameter("abstract"));
-        ref.setPublisher(req.getParameter("publisher"));
-        ref.setPublisherCity(req.getParameter("publisherCity"));
-        ref.setUrlWebReference(req.getParameter("urlWebReference"));
-        ref.setDoi(req.getParameter("doi"));
+//        ref.setTitle(req.getParameter("title"));
+//        ref.setEditors(req.getParameter("editors"));
+//        ref.setPublication(req.getParameter("publication"));
+//        ref.setVolume(req.getParameter("volume"));
+//        ref.setIssue(req.getParameter("issue"));
+//        ref.setPages(req.getParameter("pages"));
+//        ref.setNotes(req.getParameter("notes"));
+//        ref.setReferenceType(req.getParameter("referenceType"));
+//        ref.setCitation(req.getParameter("citation"));
+//        ref.setRefAbstract(req.getParameter("abstract"));
+//        ref.setPublisher(req.getParameter("publisher"));
+//        ref.setPublisherCity(req.getParameter("publisherCity"));
+//        ref.setUrlWebReference(req.getParameter("urlWebReference"));
+//        ref.setDoi(req.getParameter("doi"));
+          pro.setDesc(req.getParameter("desc"));
+          pro.setName(req.getParameter("name"));
 
         if (persist) {
             if( isNew ) {
                 // create a new rgd id
                 RGDManagementDAO rgdIdDao = new RGDManagementDAO();
-                RgdId id = rgdIdDao.createRgdId(RgdId.OBJECT_KEY_REFERENCES, "ACTIVE", "created by ReferenceEditObject", 0);
-                ref.setRgdId(id.getRgdId());
+//                RgdId id = rgdIdDao.createRgdId(RgdId.OBJECT_KEY_REFERENCES, "ACTIVE", "created by ReferenceEditObject", 0);
+                RgdId id = rgdIdDao.createRgdId(RgdId.OBJECT_KEY_EXPERIMENTS, "ACTIVE", "created by ProjectEditObject",0);
+//                ref.setRgdId(id.getRgdId());
+                pro.setRgdId(id.getRgdId());
 
-                dao.insertReference(ref);
+//                dao.insertReference(ref);
+                prodao.insertProject(pro);
             } else {
-                dao.updateReference(ref);
+//                dao.updateReference(ref);
+                prodao.updateProject(pro);
             }
 
-            updatePmid(req.getParameter("pmid"), ref.getRgdId());
+//            updatePmid(req.getParameter("pmid"), ref.getRgdId());
         }
 
-        return ref;
+        return pro;
     }
 
-    void updatePmid(String pmidParam, int refRgdId) throws Exception {
-
-        if( Utils.isStringEmpty(pmidParam) ) {
-            return; // nothing to do -- empty PMID???
-        }
-
-        // check if reference PMID is up-to-date
-        XdbIdDAO xdao = new XdbIdDAO();
-        List<XdbId> xdbIds = xdao.getPubmedIdsByRefRgdId(refRgdId);
-        for( XdbId xdbId: xdbIds ) {
-            if( Utils.stringsAreEqual(pmidParam, xdbId.getAccId()) ) {
-                return; // yes it is
-            }
-        }
-
-        // no PMID: assign PMID to this reference
-        if( xdbIds.isEmpty() ) {
-            XdbId xdbId = new XdbId();
-            xdbId.setRgdId(refRgdId);
-            xdbId.setAccId(pmidParam);
-            xdbId.setXdbKey(XdbId.XDB_KEY_PUBMED);
-            xdao.insertXdb(xdbId);
-        } else {
-            // different PMID: change PMID for this reference
-            XdbId xdbId = xdbIds.get(0);
-            xdbId.setAccId(pmidParam);
-            xdao.updateByKey(xdbId);
-        }
-    }
+//    void updatePmid(String pmidParam, int refRgdId) throws Exception {
+//
+//        if( Utils.isStringEmpty(pmidParam) ) {
+//            return; // nothing to do -- empty PMID???
+//        }
+//
+//        // check if reference PMID is up-to-date
+//        XdbIdDAO xdao = new XdbIdDAO();
+//        List<XdbId> xdbIds = xdao.getPubmedIdsByRefRgdId(refRgdId);
+//        for( XdbId xdbId: xdbIds ) {
+//            if( Utils.stringsAreEqual(pmidParam, xdbId.getAccId()) ) {
+//                return; // yes it is
+//            }
+//        }
+//
+//        // no PMID: assign PMID to this reference
+//        if( xdbIds.isEmpty() ) {
+//            XdbId xdbId = new XdbId();
+//            xdbId.setRgdId(refRgdId);
+//            xdbId.setAccId(pmidParam);
+//            xdbId.setXdbKey(XdbId.XDB_KEY_PUBMED);
+//            xdao.insertXdb(xdbId);
+//        } else {
+//            // different PMID: change PMID for this reference
+//            XdbId xdbId = xdbIds.get(0);
+//            xdbId.setAccId(pmidParam);
+//            xdao.updateByKey(xdbId);
+//        }
+//    }
 }
--- web-app/WEB-INF/jsp/curation/edit/editProject.jsp
+++ web-app/WEB-INF/jsp/curation/edit/editProject.jsp
@@ -1,1 +1,103 @@
-<h1>Hello!</h1>
+<%@ page import="edu.mcw.rgd.web.HttpRequestFacade" %>
+<%@ page import="java.text.SimpleDateFormat" %>
+<%@ page import="edu.mcw.rgd.datamodel.Reference" %>
+<%@ page import="edu.mcw.rgd.web.FormUtility" %>
+<%@ page import="edu.mcw.rgd.web.DisplayMapper" %>
+<%@ page import="edu.mcw.rgd.datamodel.RgdId" %>
+<%@ page import="java.util.List" %>
+<%@ page import="edu.mcw.rgd.dao.impl.ReferenceDAO" %>
+<%@ page import="edu.mcw.rgd.dao.impl.ProjectDAO" %>
+<%@ page import="edu.mcw.rgd.process.Utils" %>
+
+<%
+    String pageTitle = "Edit Project";
+    String headContent = "";
+    String pageDescription = "";
+%>
+
+<%@ include file="/common/headerarea.jsp"%>
+<%@ include file="editHeader.jsp" %>
+
+<%
+//    ReferenceDAO referenceDAO = new ReferenceDAO();
+    ProjectDAO projDAO = new ProjectDAO();
+//    Reference ref = (Reference) request.getAttribute("editObject");
+    Project pro1 = (Project) request.getAttribute("editObject");
+    int rgdId = pro1.getRgdId();
+
+    int displayRgdId = rgdId;
+//    int key = ref.getKey();
+
+//    String title = ref.getTitle();
+    String title = pro1.getName();
+    if (isClone) {
+//        Reference clone = (Reference) request.getAttribute("cloneObject");
+        Project clone = (Project) request.getAttribute("cloneObject");
+        pro1 = clone;
+        displayRgdId = pro1.getRgdId();
+        title = pro1.getName() + " (COPY)";
+    }
+
+//    String pmid = "";
+//    XdbIdDAO xdao = new XdbIdDAO();
+//    for( XdbId xdbId: xdao.getPubmedIdsByRefRgdId(rgdId) ) {
+//        pmid = xdbId.getAccId();
+//    }
+    String action;
+%>
+<% if( isClone ) {
+    action = "add"; %>
+<h1>Clone Project: <%=title%></h1>
+<% } else if( isNew ) {
+    action = "add"; %>
+<h1>New Project</h1>
+<% } else {
+    action = "upd"; %>
+<h1>Edit Project: <%=title%></h1>
+<% } %>
+
+<form action="editProject.html" method="get">
+    <input type="hidden" name="rgdId" value="<%=rgdId%>" />
+<%--    <input type="hidden" name="key" value="<%=key%>" />--%>
+    <input type="hidden" name="act" value="<%=action%>" />
+
+    <table>
+        <tr>
+            <td valign="top">
+                <table >
+                    <tr>
+                        <td class="label">Project ID:</td>
+                        <td><%=pro1.getRgdId()%></td>
+                    </tr>
+                    <tr>
+                        <td class="label">Project Name:</td>
+                        <td><input type="text" name="name" size="45" value="<%=dm.out("name",pro1.getName())%>" /></td>
+                    </tr>
+                    <tr>
+                        <td class="label">Project Description:</td>
+                        <td><input type="text" name="desc" size="45" value="<%=dm.out("desc",pro1.getDesc())%>" /> </td>
+                    </tr>
+
+
+                    <tr>
+                        <% if( isClone ) { %>
+                        <td colspan="2" align="center"><input type="submit" value="Clone Project"/></td>
+                        <% } else if( isNew ) { %>
+                        <td colspan="2" align="center"><input type="submit" value="Insert Project"/></td>
+                        <% } else { %>
+                        <td colspan="2" align="center"><input type="button" value="Update Project" onclick="makePOSTRequest(this.form)"/></td>
+                        <% } %>
+                    </tr>
+
+
+                </table>
+            </td>
+            <td>&nbsp;</td>
+            <td valign="top" align="center">
+                <%@ include file="idInfo.jsp"%>
+            </td>
+        </tr>
+    </table>
+</form>
+
+<%@ include file="/common/footerarea.jsp"%>
