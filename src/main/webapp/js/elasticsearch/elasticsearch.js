05ada8a66a9b1cb4ed3be2406f846b47a1100c57
Thota <jthota@mcw.edu>
2024-01-18 15:16:11 -0600

    fixed js

--- src/main/webapp/js/elasticsearch/elasticsearch.js
+++ src/main/webapp/js/elasticsearch/elasticsearch.js
@@ -333,18 +333,11 @@ function filterClick(category, species,subCat, type, filter, objectAssembly) {
     objectAssembly= $objectAssembly;
 
     var $sampleExists;
-    var filterType;
-
-    if(filter=='trait'){
-        filterType='trait';
-     }else if(filter=='polyphenStatus'){
-        filterType='polyphenStatus'
-    } else if(filter=='sample'){
-    filterType='sample'
-} else if(filter=='region'){
-    filterType='region'
-} else{
-        filterType='type'
+    var filterType=null;
+    if ((typeof filter != 'undefined') && filter!=''){
+        filterType=filter;
+    }else{
+        filterType='type';
     }
 
     $('#subCat').val(subCat);
