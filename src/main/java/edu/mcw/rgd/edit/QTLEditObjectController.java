c2a41db10fc0b6d78afce5346d3af69dc088a38d
tutajm <mtutaj@mcw.edu>
2024-03-12 14:52:51 -0500

    qtl object edit: fix when saving flanking/peak markers

--- src/main/java/edu/mcw/rgd/edit/QTLEditObjectController.java
+++ src/main/java/edu/mcw/rgd/edit/QTLEditObjectController.java
@@ -130,15 +130,29 @@ public class QTLEditObjectController extends EditObjectController {
             qtl.setVariance(null);
         }
 
-        if (this.checkInteger("Flank 1 RGD ID", req.getParameter("flank1RgdId"), false)) {
-            qtl.setFlank1RgdId(Integer.parseInt(req.getParameter("flank1RgdId")));
+        String flank1RgdIdStr = request.getParameter("flank1RgdId");
+        if( flank1RgdIdStr!=null ) {
+            int flank1RgdId = 0;
+            try {
+                flank1RgdId = Integer.parseInt(flank1RgdIdStr);
+            } catch( NumberFormatException e) {}
+            qtl.setFlank1RgdId( flank1RgdId > 0 ? flank1RgdId : null );
         }
-        if (this.checkInteger("Flank 2 RGD ID", req.getParameter("flank2RgdId"), false)) {
-            qtl.setFlank2RgdId(Integer.parseInt(req.getParameter("flank2RgdId")));
+        String flank2RgdIdStr = request.getParameter("flank2RgdId");
+        if( flank2RgdIdStr!=null ) {
+            int flank2RgdId = 0;
+            try {
+                flank2RgdId = Integer.parseInt(flank2RgdIdStr);
+            } catch( NumberFormatException e) {}
+            qtl.setFlank2RgdId( flank2RgdId > 0 ? flank2RgdId : null );
         }
-
-        if (this.checkInteger("Peak RGD ID", req.getParameter("peakRgdId"), false)) {
-            qtl.setPeakRgdId(Integer.parseInt(req.getParameter("peakRgdId")));
+        String peakRgdIdStr = request.getParameter("peakRgdId");
+        if( peakRgdIdStr!=null ) {
+            int peakRgdId = 0;
+            try {
+                peakRgdId = Integer.parseInt(peakRgdIdStr);
+            } catch( NumberFormatException e) {}
+            qtl.setPeakRgdId( peakRgdId > 0 ? peakRgdId : null );
         }
 
         qtl.setInheritanceType(req.getParameter("inheritanceType"));
@@ -151,6 +165,7 @@ public class QTLEditObjectController extends EditObjectController {
             if (isNew) {
                 dao.insertQTL(qtl, req.getParameter("objectStatus"), SpeciesType.parse(req.getParameter("speciesType")));
             } else {
+                //System.out.println("QTL UPDATE FLANK1="+qtl.getFlank1RgdId()+", FLANK2="+qtl.getFlank2RgdId()+", PEAK="+qtl.getPeakRgdId());
                 dao.updateQTL(qtl);
                 addNomenEvents(nomenEvents);
                 insertAliases(aliases);
