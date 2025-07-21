58abdde608ddaef9e230a5aef52485fc96170fa5
jdepons <jdepons@mcw.edu>
2025-07-21 12:35:09 -0500

    changed expires to 23 gmt

--- src/main/java/edu/mcw/rgd/generator/ListGeneratorController.java
+++ src/main/java/edu/mcw/rgd/generator/ListGeneratorController.java
@@ -161,7 +161,7 @@ public class ListGeneratorController implements Controller {
 
         Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
         calendar.setTime(new Date());
-        calendar.set(Calendar.HOUR_OF_DAY, 9);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
