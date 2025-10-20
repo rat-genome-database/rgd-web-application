e050197a3bba85d54cbe4c52a1e3f2dde44c87b4
tutajm <mtutaj@mcw.edu>
2025-10-20 16:34:01 -0500

    GeneReportController: removed unused imports

--- src/main/java/edu/mcw/rgd/report/GeneReportController.java
+++ src/main/java/edu/mcw/rgd/report/GeneReportController.java
@@ -1,26 +1,16 @@
 package edu.mcw.rgd.report;
 
 import edu.mcw.rgd.dao.impl.GeneDAO;
-import edu.mcw.rgd.web.HttpRequestFacade;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.springframework.web.servlet.ModelAndView;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 /**
- * Created by IntelliJ IDEA.
  * User: jdepons
  * Date: Jun 2, 2008
- * Time: 8:59:47 AM
  */
 public class GeneReportController extends ReportController {
 
 
     public String getViewUrl() throws Exception {
        return "gene/main.jsp";
-
     }
 
     public Object getObject(int rgdId) throws Exception{
