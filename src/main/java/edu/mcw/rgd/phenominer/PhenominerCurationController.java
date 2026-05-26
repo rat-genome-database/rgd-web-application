b3f89583512c59ca56ad9bab82d1caf97d53a298
Akhilanand K <kundurthiakhil@gmail.com>
2026-05-26 11:22:02 -0500

    added Add Unit tool to PhenoMiner Curation home page

--- src/main/java/edu/mcw/rgd/phenominer/PhenominerCurationController.java
+++ src/main/java/edu/mcw/rgd/phenominer/PhenominerCurationController.java
@@ -1,12 +1,14 @@
 package edu.mcw.rgd.phenominer;
 
 import edu.mcw.rgd.dao.impl.PhenominerDAO;
+import edu.mcw.rgd.datamodel.pheno.Enumerable;
 import edu.mcw.rgd.web.HttpRequestFacade;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,6 +33,44 @@ public class PhenominerCurationController extends PhenominerController {
 
         if (!req.getParameter("referenceId").equals("")) {
             status.add("Reference imported with RGD ID " + req.getParameter("referenceId"));
+        }
+
+        if (action.equals("addUnit")) {
+            try {
+                String unitType = request.getParameter("unitType");
+                String unitValue = request.getParameter("unitValue");
+                String description = request.getParameter("description");
+
+                Enumerable e = new Enumerable();
+                e.setType(Integer.parseInt(unitType));
+                e.setValue(unitValue);
+                e.setLabel(unitValue);
+                e.setDescription(description);
+
+                if (Integer.parseInt(unitType) == 3) {
+                    List<String> existingCMO = dao.getDistinct("PHENOMINER_ENUMERABLES where type=3  ", "label", true);
+                    if (!existingCMO.contains(unitValue)) {
+                        if (unitValue != null && !unitValue.equals(""))
+                            dao.insertEnumerable(e);
+                    }
+                    String termAcc = request.getParameter("accId");
+                    if (request.getParameterMap().containsKey("termScale") && request.getParameter("termScale") != null && !request.getParameter("termScale").trim().isEmpty()) {
+                        String termScale = request.getParameter("termScale");
+                        dao.insertUnitConversion(termAcc, unitValue, termScale);
+                    } else {
+                        dao.checkUnitConversion(termAcc, unitValue);
+                    }
+                } else {
+                    List<String> existingXCO = dao.getDistinct("PHENOMINER_ENUMERABLES where type=2  ", "label", true);
+                    if (!existingXCO.contains(unitValue)) {
+                        if (unitValue != null && !unitValue.equals(""))
+                            dao.insertEnumerable(e);
+                    }
+                }
+                status.add("Unit added successfully");
+            } catch (Exception e) {
+                error.add("Error adding unit: " + e.getMessage());
+            }
         }
 
         request.setAttribute("error", error);
--- src/main/webapp/WEB-INF/jsp/curation/phenominer/home.jsp
+++ src/main/webapp/WEB-INF/jsp/curation/phenominer/home.jsp
@@ -37,8 +37,60 @@
     <tr>
         <td><a href="phenominerUnitTables.html"><li>View Phenominer Unit Tables</a> </td>
     </tr>
+    <tr>
+        <td><a href="javascript:addUnit()"><li>Add Unit</a> </td>
+    </tr>
 </table>
 
+<script type="text/javascript" src="/rgdweb/js/ontologyLookup.js"></script>
+<link rel="stylesheet" href="/rgdweb/js/windowfiles/dhtmlwindow.css" type="text/css"/>
+<script type="text/javascript" src="/rgdweb/js/windowfiles/dhtmlwindow.js"></script>
+
+<script>
+    function addUnit() {
+        var unit = document.getElementById("unit");
+        unit.style.display = "block";
+    }
+    function addSD() {
+        var ontId = document.getElementById("accId").value;
+        if (ontId != null && ontId != "") {
+            $.ajax({
+                url: "/rgdws/lookup/standardUnit/" + ontId.replace(":", "%3A"), success: function (result) {
+                    document.getElementById("unitSD").value = result;
+                }
+            });
+        }
+    }
+    function checkUnitConversion() {
+        var SD = document.getElementById("unitSD").value;
+        var unitValue = document.getElementById("unitValue").value;
+
+        if (SD != null && SD != unitValue && SD != "")
+            document.getElementById("termScale").style.display = "block";
+    }
+</script>
+<br>
+<div id="unit" style="display:none;">
+    <form name="addUnitForm" action="home.html" method="get">
+        <input type="hidden" name="action" value="addUnit"/>
+        <b>Add Unit</b>
+        Unit Type: <select name="unitType" id="unitType">
+            <option value="3">CMO Unit</option>
+            <option value="2">Experiment Unit</option>
+        </select>
+        *ACC ID <input type="text" id="accId" name="accId" size="40" value="" onchange="addSD()"/>
+        <a href="javascript:lookup_treeRender('accId', 'CMO', 'CMO:0000000')"><img src="/rgdweb/common/images/tree.png"
+                                                                                   border="0"/></a>
+        Standard Unit <input id="unitSD" name="unitSD" style="background-color: #dddddd"
+                             readonly="true">
+        <br>
+        *New Unit <input name="unitValue" id="unitValue" onchange="checkUnitConversion()">
+        Description <input name="description">
+        <br>
+        <input id="termScale" style="display:none;" name="termScale" placeholder="Term Specific Scale">
+        <button type="submit">Add</button>
+    </form>
+</div>
 
 <%
     if (req.getParameter("fix").equals("1")) {
